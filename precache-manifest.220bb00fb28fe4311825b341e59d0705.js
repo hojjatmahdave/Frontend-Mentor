d2907f2ba23f4b05ca6cc023fd48364eb83d3f66
Mohammad Hojjat <hojjatjanjan@gmail.com>
2023-05-31 09:49:02 +0430

    Updates

---
+++ precache-manifest.220bb00fb28fe4311825b341e59d0705.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "94cceaf6c31946041640",
+    "url": "/portfolio-website/static/css/main.e43241ba.chunk.css"
+  },
+  {
+    "revision": "94cceaf6c31946041640",
+    "url": "/portfolio-website/static/js/main.07d5f23e.chunk.js"
+  },
+  {
+    "revision": "0e1e731819752b5e1982",
+    "url": "/portfolio-website/static/js/runtime~main.7784a2cd.js"
+  },
+  {
+    "revision": "2b3a5f4c8b8055fd27b2",
+    "url": "/portfolio-website/static/css/2.71bbc07f.chunk.css"
+  },
+  {
+    "revision": "2b3a5f4c8b8055fd27b2",
+    "url": "/portfolio-website/static/js/2.29264429.chunk.js"
+  },
+  {
+    "revision": "8720c82b6b050d12486f63e0e8aaac1b",
+    "url": "/portfolio-website/static/media/logo-white.8720c82b.svg"
+  },
+  {
+    "revision": "ca40f3d9be2961ccb9704226af4b58c2",
+    "url": "/portfolio-website/static/media/stars-5.ca40f3d9.svg"
+  },
+  {
+    "revision": "d3fcaae4cf243731a7f555f4b146aedd",
+    "url": "/portfolio-website/static/media/account.d3fcaae4.jpg"
+  },
+  {
+    "revision": "ee0ac5bd035aded233fa8e648657b647",
+    "url": "/portfolio-website/static/media/logo_appstores_androidgoogleplay_en.ee0ac5bd.svg"
+  },
+  {
+    "revision": "e75c5893863f4a50caf48f27174f9fbc",
+    "url": "/portfolio-website/static/media/logo_appstores_iosappstore_en.e75c5893.svg"
+  },
+  {
+    "revision": "dc37f9f0e88296a6c0f8dbf97794b585",
+    "url": "/portfolio-website/static/media/app.dc37f9f0.webp"
+  },
+  {
+    "revision": "98bed0e05e6e89f7f697f7589bacce05",
+    "url": "/portfolio-website/static/media/globe.98bed0e0.svg"
+  },
+  {
+    "revision": "f68daa1f58b0e495b98b263c28dbc3e8",
+    "url": "/portfolio-website/static/media/nohistory.f68daa1f.svg"
+  },
+  {
+    "revision": "9296c296c538654177cb9b4c90fcd1a1",
+    "url": "/portfolio-website/static/media/charity-cards.9296c296.png"
+  },
+  {
+    "revision": "d399a6c501cb23e196b4f0e2170046b6",
+    "url": "/portfolio-website/static/media/Central-North_America.d399a6c5.webp"
+  },
+  {
+    "revision": "95a363755f9f4be77f2ec945d8b272ff",
+    "url": "/portfolio-website/static/media/star-rating-45.95a36375.svg"
+  },
+  {
+    "revision": "7289ccdc7a2e7f723b8ebdb86afc7d26",
+    "url": "/portfolio-website/index.html"
+  }
+];
