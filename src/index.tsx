f2e3138fccb3c2ec141bb635cf9fec02c938ab75
Mohammad Hojjat <hojjatjanjan@gmail.com>
2024-06-10 21:22:40 +0430

    create new branch

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./news-homepage-main/container/app";
 
 // import "./index.css";
 import { GlobalStyles } from "./news-homepage-main/styles/gloabal";
@@ -9,6 +8,7 @@ import RoutePage from "./interactive-rating-component-main/components/RoutePage"
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <BrowserRouter>
     <GlobalStyles />
